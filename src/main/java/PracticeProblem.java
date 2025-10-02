be66243ba5435f8a53ae58249bf75439106c5662
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-10-02 21:00:35 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,54 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
-
-	
-
+//Question 1
+public static String readFile(String filename) {
+FileReader inputStream = null;
+StringBuilder builder = new StringBuilder();
+try {
+	inputStream = new FileReader(filename); 
+	int c;
+	while ((c = inputStream.read()) != -1) {
+	builder.append((char) (c));
+	}
+} catch (IOException e) {
+		System.out.println(e); 
+} finally {
+		try {
+			if (inputStream != null) {
+ 			inputStream.close();
+		}
+ 	} catch (IOException e) {
+		System.out.println("Error closing"); //
+	}
+}
+return builder.toString();
+}
+//Question 2
+public static String backwardsReadFile(String filename) {
+	FileReader reader = null;
+	StringBuilder builder = new StringBuilder();
+	try {
+		reader = new FileReader(filename);
+		int c;
+		while ((c = reader.read()) != -1) {
+		builder.append((char) (c));	}
+		} catch (IOException e) {
+		System.out.println(e); 
+		} finally {
+			try {
+				if (reader != null) {
+ 				reader.close();
+		}
+ 	} catch (IOException e) {
+		System.out.println("Error closing"); //
+	}
+}
+return builder.reverse().toString();
+}
 }
